81b0ad3d8430b49ab94087baf4140d3853b1867a
Carey Johnson <johnson.careyj@gmail.com>
2018-08-07 23:32:48 -0700

    changed the size of the shapes and made the shapes to the background images of the divs

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ canvas.width = window.innerWidth;
 
 let putShape = function(e) {
   let shape = new Image();
-  shape.src = "shapes_pics/hexagon.png";
+  shape.src = "../shapes_pics/orangesquare.png";
   context.drawImage(shape, e.clientX - shape.width/2, e.clientY - shape.height/2);
   
 };
-
+let coords = function(e) {
+  console.log("hey");
+};
 
 
 canvas.addEventListener("mousedown", putShape);
+// canvas.addEventListener("mousemove", coords);
 // canvas.addEventListener("mousemove", dragShape);
 
